c3d48cf5fd5ff735e97ed7530efbd9dc8719e493
Obrien Troy Dangan <202080491@psu.palawan.edu.ph>
2024-10-19 20:11:00 +0800

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -105,7 +105,7 @@ export default function Header(props) {
                 </div>
             </header>
             <main>
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"> {props.children}</div>
+                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 border"> {props.children}</div>
             </main>
         </div>
 
